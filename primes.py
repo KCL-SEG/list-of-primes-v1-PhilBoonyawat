88fb6618ac820c49f5a829f380e32bbf5a246b10
Boonyawat Ariyawatkul <boonyawat.ariyawatkul@kcl.ac.uk>
2023-09-24 21:44:16 +0100

    Add functionality to primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    list.append(num)
+    num = num + 1
+    while len(list) < number_of_primes:
+        for divisor in range(2,num):
+            if num % divisor == 0:
+                break
+        else:
+            list.append(num)
+        num += 1
+
     return list
